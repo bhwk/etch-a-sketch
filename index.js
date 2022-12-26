339a871905f186a96e908cb9e5c74cf84d463f75
bhwk <120708160+bhwk@users.noreply.github.com>
2022-12-26 18:20:26 +0800

    Setup skeleton for automatic grid generation

---
+++ index.js
@@ -0,0 +1,22 @@
+const container = document.getElementById("container");
+
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    // cell.innerText = (c + 1);
+    container.appendChild(cell).className = "grid-item";
+  };
+};
+
+function changeColor(item){
+}
+
+makeRows(16, 16);
+
+const gridItem = document.querySelectorAll('.grid-item');
+
+gridItem.forEach((item)=> {
+    item.addEventListener('mouseover', changeColor(item));
+});
